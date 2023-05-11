c495bb2e689467f12ac37639d54ca1d6d361e56c
Aron <aronlee5263@gmail.com>
2023-05-11 19:55:01 +0900

    get post 요청

--- fetch/server/app.js
+++ fetch/server/app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 
+let id = 2;
+const todoList = [
+  {
+    id: 1,
+    text: "밥먹기",
+    done: false,
+  },
+];
+
 app.get("/", function (req, res) {
   res.send("Hello World 서버 돌아가?");
+});
+
+app.get("/api/todo", () => {
+  return todoList;
+});
+
+app.post("/api/todo", (req, res) => {
+  const { text, done } = req.body;
 });
 
 app.listen(3000, () => {
