dd74c173501e944983484af143cb84211c1f9747
Aron <aronlee5263@gmail.com>
2023-05-11 22:20:52 +0900

    - fetch 예시 - 불필요한 이미지 삭제

--- fetch/client/src/App.jsx
+++ fetch/client/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 
 function App() {
+  fetch("http://localhost:4000/api/todo")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
   return (
     <div className="App">
       <h1>TODO LIST</h1>
