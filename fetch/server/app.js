b4bb0c9e5e73bff7e014d4d7b691696f2733e55c
Aron <aronlee5263@gmail.com>
2023-05-11 22:21:20 +0900

    CORS 허용 (백엔드에서)
    npm install cors

--- fetch/server/app.js
+++ fetch/server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use(cors());
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
