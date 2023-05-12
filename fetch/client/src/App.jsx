386fb6bb18b8523ce7bc8a9083d9ea1772441016
Aron <aronlee5263@gmail.com>
2023-05-12 22:35:21 +0900

    서버에 보낸 http요청중에서 response랑 data 콘솔에 찍어봄

--- fetch/client/src/App.jsx
+++ fetch/client/src/App.jsx
@@ -2,20 +2,50 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  let cnt = 1;
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch("http://localhost:5000/api/todo");
-      const json = await response.json();
-      setTodoList(json);
-    }
-    fetchData();
+    fetch("http://localhost:5000/api/todo")
+      .then((response) => {
+        console.log("response", cnt, "번 째 : ", response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data ", cnt, "번 째 : " + JSON.stringify(data));
+        setTodoList(data);
+      })
+      .catch((error) => {
+        console.error("에러 발생:", error);
+      });
+    cnt = cnt + 1;
   }, []);
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    const text = e.target.text.value;
+    const done = e.target.checked;
+    fetch("http://localhost:5000/api/todo", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text,
+        done,
+      }),
+    });
+  };
 
   return (
     <div>
       <h1>TODO LIST</h1>
-      {todoList.map((todo) => {
+      <form onSubmit={onSubmitHandler}>
+        <input name="text" />
+        <input name="text" type="checkbox" />
+        <input type="submit" value="추가" />
+      </form>
+
+      {todoList?.map((todo) => {
         return (
           <div key={todo.id}>
             <li>{"id : " + todo.id}</li>
