3ee1bd5868dcfab9dbd70c4c6e3662812fb4e14f
Aron <aronlee5263@gmail.com>
2023-05-15 23:07:27 +0900

    axios delete 핸들러 삭제

--- fetch/client/src/components/App.jsx
+++ fetch/client/src/components/App.jsx
@@ -25,14 +25,6 @@ function App() {
     axiosData();
   };
 
-  const onSubmitDeleteHandler = async (e) => {
-    e.preventDefault();
-    const text = e.target.text.value;
-    const done = e.target.done.checked;
-    await axios.delete(SERVER_URL, {});
-    axiosData();
-  };
-
   return (
     <>
       <div className={classes.header_container}>
