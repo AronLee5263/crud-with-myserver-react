e70ee17ac2583ee6dc6ac59e6687cc10e2dfa052
Aron <aronlee5263@gmail.com>
2023-05-16 23:25:30 +0900

    create.jsx에서 useState 훅 추가

--- fetch/client/src/components/Create.jsx
+++ fetch/client/src/components/Create.jsx
@@ -1,21 +1,27 @@
-import React from "react";
+import { useState } from "react";
 import { Button, Checkbox, Form } from "semantic-ui-react";
 
-const Create = () => (
-  <Form className="create-form">
-    <Form.Field>
-      <label>First Name</label>
-      <input placeholder="First Name" />
-    </Form.Field>
-    <Form.Field>
-      <label>Last Name</label>
-      <input placeholder="Last Name" />
-    </Form.Field>
-    <Form.Field>
-      <Checkbox label="I agree to the Terms and Conditions" />
-    </Form.Field>
-    <Button type="submit">Submit</Button>
-  </Form>
-);
+const Create = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [checkbox, setCheckbox] = useState(false);
+
+  return (
+    <Form className="create-form">
+      <Form.Field>
+        <label>First Name</label>
+        <input placeholder="First Name" />
+      </Form.Field>
+      <Form.Field>
+        <label>Last Name</label>
+        <input placeholder="Last Name" />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label="I agree to the Terms and Conditions" />
+      </Form.Field>
+      <Button type="submit">Submit</Button>
+    </Form>
+  );
+};
 
 export default Create;
