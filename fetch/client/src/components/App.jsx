0f66d703b5910352143096735b73d401fb128908
Aron <aronlee5263@gmail.com>
2023-05-16 22:23:53 +0900

    셋업 완료
    - 글꼴 적용
    - c,r,u  .jsx 파일 만듦
    
    - 아래 url에 나온 방법대로 따라함.
         https://www.freecodecamp.org/news/how-to-perform-crud-operations-using-react/

--- fetch/client/src/components/App.jsx
+++ fetch/client/src/components/App.jsx
@@ -1,7 +1,11 @@
 import classes from "./App.module.css";
 
 function App() {
-  return <div className={classes.main}>React Crud Operations</div>;
+  return (
+    <div className={classes.main}>
+      <h2 className={classes.main_header}>React Crud Operations</h2>
+    </div>
+  );
 }
 
 export default App;
