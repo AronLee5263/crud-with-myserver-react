0d3f953202f317fc9bce5d74a4daadb1b7556fc0
Aron <aronlee5263@gmail.com>
2023-05-16 23:45:15 +0900

    useState 를 이용해 상태 업데이트 및 콘솔에 찍어보기

--- fetch/client/src/components/Create.jsx
+++ fetch/client/src/components/Create.jsx
@@ -6,20 +6,43 @@ const Create = () => {
   const [lastName, setLastName] = useState("");
   const [checkbox, setCheckbox] = useState(false);
 
+  function changeFirstNameHandler(event) {
+    setFirstName(event.target.value);
+  }
+
+  function changeLastNameHandler(event) {
+    setLastName(event.target.value);
+  }
+
+  function changeCheckboxHandler(event) {
+    setCheckbox(!checkbox);
+  }
+
+  const postData = () => {
+    console.log(firstName);
+    console.log(lastName);
+    console.log(checkbox);
+  };
+
   return (
     <Form className="create-form">
       <Form.Field>
         <label>First Name</label>
-        <input placeholder="First Name" />
+        <input placeholder="First Name" onChange={changeFirstNameHandler} />
       </Form.Field>
       <Form.Field>
         <label>Last Name</label>
-        <input placeholder="Last Name" />
+        <input placeholder="Last Name" onChange={changeLastNameHandler} />
       </Form.Field>
       <Form.Field>
-        <Checkbox label="I agree to the Terms and Conditions" />
+        <Checkbox
+          label="I agree to the Terms and Conditions"
+          onChange={changeCheckboxHandler}
+        />
       </Form.Field>
-      <Button type="submit">Submit</Button>
+      <Button type="submit" onClick={postData}>
+        Submit
+      </Button>
     </Form>
   );
 };
