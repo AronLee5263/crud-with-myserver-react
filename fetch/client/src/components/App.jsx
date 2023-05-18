8aca615dc10cdeaa733de77f3300d6a029713e74
Aron <aronlee5263@gmail.com>
2023-05-18 23:15:27 +0900

    라우터 import 래핑하기

--- fetch/client/src/components/App.jsx
+++ fetch/client/src/components/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import Create from "./Create";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="main">
-      <h2 className="main-header">React Crud Operations</h2>
-      <div>
-        <Create />
+    <Router>
+      <div className="main">
+        <h2 className="main-header">React Crud Operations</h2>
+        <div>
+          <Create />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
