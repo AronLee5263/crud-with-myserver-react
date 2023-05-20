7cbf04d342f6da3f1480e99b693ca22ee45f1480
Aron <aronlee5263@gmail.com>
2023-05-20 19:16:08 +0900

    라우터 추가
    - App.jsx에서 라우터 삭제
    - main.jsx에 라우터 추가
    
    리액트 라우터 공식 홈페이지 튜토리얼 피셜임
    https://reactrouter.com/en/main/start/tutorial

--- fetch/client/src/components/App.jsx
+++ fetch/client/src/components/App.jsx
@@ -1,17 +1,14 @@
 import "./App.css";
 import Create from "./Create";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="main">
-        <h2 className="main-header">React Crud Operations</h2>
-        <div>
-          <Create />
-        </div>
+    <div className="main">
+      <h2 className="main-header">React Crud Operations</h2>
+      <div>
+        <Create />
       </div>
-    </Router>
+    </div>
   );
 }
 
--- fetch/client/src/main.jsx
+++ fetch/client/src/main.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./components/App";
 import "./index.css";
 import "semantic-ui-css/semantic.min.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
