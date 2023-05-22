3624e1ee84e0bfd2fb520de7f5c0d0cc017f6aec
Aron <aronlee5263@gmail.com>
2023-05-22 18:52:58 +0900

    main에 라우트 속성 추가
    create 컴포넌트 컴포넌트 형식에 맞게 변경

--- fetch/client/src/main.jsx
+++ fetch/client/src/main.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./components/App";
+import Create from "./components/Create";
 
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/create",
+    element: <Create />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
