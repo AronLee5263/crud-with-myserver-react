fc91f9349f26e52491ea04cf3ef84bcd54a33105
Aron <aronlee5263@gmail.com>
2023-05-22 20:04:35 +0900

    Read 컴포넌트에 테이블 추가

--- fetch/client/src/components/Read.jsx
+++ fetch/client/src/components/Read.jsx
@@ -1,4 +1,5 @@
 import classes from "./Read.module.css";
+import { Table } from "semantic-ui-react";
 
 export default function Read() {
   return (
@@ -14,6 +15,28 @@ export default function Read() {
           <input name="done" type="checkbox" />
           <input type="submit" value="추가" />
         </form>
+      </div>
+
+      <div>
+        <Table singleLine>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Registration Date</Table.HeaderCell>
+              <Table.HeaderCell>E-mail address</Table.HeaderCell>
+              <Table.HeaderCell>Premium Plan</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>John Lilki</Table.Cell>
+              <Table.Cell>September 14, 2013</Table.Cell>
+              <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
+              <Table.Cell>No</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
       </div>
     </>
   );
