da58b7b06f4d3cadfd2f53d3a1af28d5e0f7076e
Aron <aronlee5263@gmail.com>
2023-05-24 18:19:13 +0900

    Post 작업 완료 (서버에 데이터 들어감)
    - console.log 찍어서 먼저 브라우저에 찎히는지 프론트쪽에서 확인하고
    - axios import 한 다음 데이터 보내고 fakeData에서 보내진 데이터 확인 완료

--- fetch/client/src/components/App.jsx
+++ fetch/client/src/components/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 
 import classes from "./App.module.css";
 
--- fetch/client/src/routes/Create.jsx
+++ fetch/client/src/routes/Create.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, Checkbox, Form } from "semantic-ui-react";
+import axios from "axios";
 
 import classes from "./Create.module.css";
 
@@ -7,15 +8,21 @@ import BackButton from "../components/BackButton";
 import MainHeader from "../components/Mainheader";
 import UnderMainHeader from "../components/UnderMainHeader";
 
+const SERVER_URL = "https://64637a9f7a9eead6fae801e2.mockapi.io/fakeData";
+
 export default function Create() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [checkbox, setCheckbox] = useState(false);
+
   const postData = () => {
     console.log(firstName);
     console.log(lastName);
     console.log(checkbox);
+
+    axios.post(SERVER_URL, { firstName, lastName, checkbox });
   };
+
   return (
     <>
       <BackButton />
