62289e3eaa9584fe06e9a6a50fe91d5ce701e669
Aron <aronlee5263@gmail.com>
2023-05-25 19:20:41 +0900

    깃헙 gh-pages 배포 작업 경로 설정
    - 위해서 createBrowserRouter에  basename 속성 추가

--- fetch/client/src/main.jsx
+++ fetch/client/src/main.jsx
@@ -11,30 +11,36 @@ import Read from "./routes/Read.jsx";
 import Update from "./routes/Update.jsx";
 import BackButton from "./components/BackButton.jsx";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/create",
+      element: <Create />,
+    },
+    ,
+    {
+      path: "/read",
+      element: <Read />,
+    },
+    ,
+    {
+      path: "/update",
+      element: <Update />,
+    },
+    {
+      path: "../",
+      element: <BackButton />,
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/create",
-    element: <Create />,
-  },
-  ,
-  {
-    path: "/read",
-    element: <Read />,
-  },
-  ,
-  {
-    path: "/update",
-    element: <Update />,
-  },
-  {
-    path: "../",
-    element: <BackButton />,
-  },
-]);
+    basename: "https://aronlee5263.github.io/crud-with-myserver-react/",
+  }
+);
+<Link to="/" />; // results in <a href="/app" />
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
