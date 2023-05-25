8e225d6c7f4c3d3daa155934c9341e7f8a5f0eb7
이정운 <aronlee5263@gmail.com>
2023-05-25 21:26:05 +0900

    Delete fetch directory

--- fetch/client/src/components/UnderMainHeader.jsx
+++
@@ -1,9 +0,0 @@
-import classes from "./UnderMainHeader.module.css";
-
-export default function UnderMainHeader() {
-  return (
-    <div className={classes.main}>
-      <h4 className={classes.main_header}>React Crud Operations</h4>
-    </div>
-  );
-}
