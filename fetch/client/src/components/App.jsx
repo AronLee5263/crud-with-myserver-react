5124bc75048cb83063cfa500c2cf5cc65aebe16d
Aron <aronlee5263@gmail.com>
2023-05-29 01:12:24 +0900

    5.28 라우터 삭제하기 전에 원래 메인 브랜치 내용 입니다.

--- fetch/client/src/components/App.jsx
+++ fetch/client/src/components/App.jsx
@@ -7,7 +7,8 @@ import CrudButtons from "./CrudButtons";
 import UnderMainHeader from "./UnderMainHeader";
 
 const SERVER_URL = "";
-// 브랜치 테스트
+// 5.28 라우터 삭제하기 전에 원래 메인 브랜치 내용 입니다.
+// 지금부터는 이 브랜치에 메인 내용을 전부 담아 놓을거고 실제 메인 브랜치에서는 맥스 프로젝트 참고해서 시작
 
 function App() {
   return (
