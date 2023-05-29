842258750adcbb911d29b1860542ec52d95fb9f9
Aron <aronlee5263@gmail.com>
2023-05-29 20:45:26 +0900

    PostsList 컴포넌트 스타일 수정

--- fetch/client/src/components/PostsList.jsx
+++ fetch/client/src/components/PostsList.jsx
@@ -1,10 +1,10 @@
 import Post from "./Post";
 
-// import classes from "./PostList.module.css";
+import classes from "./PostsList.module.css";
 
 export default function PostsList() {
   return (
-    <ul>
+    <ul className={classes.posts}>
       <Post author="정운" body="여기 출력" />
       <Post author="미민" body="미미미민" />
     </ul>
