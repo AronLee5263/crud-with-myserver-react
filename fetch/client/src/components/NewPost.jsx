47641745bca975572db99c2edd257abe1f4a3e37
Aron <aronlee5263@gmail.com>
2023-05-30 23:39:10 +0900

    NewPost 컴포넌트 스타일 업데이트
    (크기 키우고 트위터랑 비슷하게 만듬)

--- fetch/client/src/components/NewPost.jsx
+++ fetch/client/src/components/NewPost.jsx
@@ -2,46 +2,39 @@ import classes from "./NewPost.module.css";
 
 function NewPost(props) {
   return (
-    <div className={classes.container}>
-      <form className={classes.form}>
-        <p>
-          <label htmlFor="postAuthor">이름</label>
-          <input
-            id="postAuthor"
-            required
-            type="text"
-            onChange={props.onAuthorChange}
-          />
-        </p>
-        <p>
-          <label htmlFor="postContent">내용</label>
-          <textarea
-            id="postContent"
-            required
-            rows={3}
-            onChange={props.onContentChange}
-          />
-        </p>
+    <form className={classes.form}>
+      <p className={classes.actions}>
+        <button type="reset">취소</button>
+        <button type="submit">업로드</button>
+      </p>
+      <p>
+        <label htmlFor="postAuthor">이름</label>
+        <input
+          className={classes.postAuthor}
+          id="postAuthor"
+          required
+          type="text"
+          onChange={props.onAuthorChange}
+        />
+      </p>
+      <p>
+        <label htmlFor="postContent">내용</label>
+        <textarea
+          className={classes.postContent}
+          id="postContent"
+          required
+          rows={9}
+          placeholder="무슨 일이 일어나고 있나요?"
+          onChange={props.onContentChange}
+        />
+      </p>
 
-        <label>
-          <input type="checkbox" name="myCheckbox" />
-          <span className={classes.checkboxBody}>(필수) 서비스 이용약관</span>
-        </label>
-        <hr />
-        <button type="reset">초기화</button>
-        <button type="submit">저장</button>
-      </form>
-    </div>
+      <label>
+        <input type="checkbox" name="myCheckbox" />
+        <span className={classes.checkboxBody}>(필수) 서비스 이용약관</span>
+      </label>
+    </form>
   );
 }
 
 export default NewPost;
-
-//1. 이벤트 리스너 만들고 (이벤트 리스너 event 는 자동으로 리액트가 객체로 넘겨줌)
-// 2. 동적으로 textarea 속성으로 이벤트 리스너 넣어주기
-// 3. useState로 등록.
-//       - 2개 배열 반환함 [현재 상태값, 상태업데이트 함수]
-//       - 이벤트리스너 함수에서 변수에 담는게 아니라 상태업데이트 함수에 매개변수로 넣어줌.
-// 왜냐하면 규칙임
-//       - const로 설정한 이유는 onChange 이벤트로 매번 키가 눌릴때마다 새롭게 컴포넌트 함수가 실행돼서
-//          항상 새로운 const 가 되기 때문임.
