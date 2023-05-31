fdd9df364462c3ad3ccde6c8ba4b6d778da46323
Aron <aronlee5263@gmail.com>
2023-06-01 01:55:10 +0900

    매우 중요 /  axios로 백엔드에 데이터 보내기
    서버에 보내는 필드부분 그러니까 key : value에서 key 부분이
    19번째줄 name, content임

--- fetch/client/src/components/PostsList.jsx
+++ fetch/client/src/components/PostsList.jsx
@@ -3,12 +3,25 @@ import Modal from "./Modal";
 import NewPost from "./NewPost";
 import Post from "./Post";
 
+import axios from "axios";
+
 import classes from "./PostsList.module.css";
 
 export default function PostsList({ isEditing, onCloseModal }) {
   const [posts, setPosts] = useState([]);
 
+  const SERVER_URL = "https://64637a9f7a9eead6fae801e2.mockapi.io/fakeData";
+
   function addPostHandler(postData) {
+    axios
+      .post(SERVER_URL, {
+        name: postData.author,
+        content: postData.body,
+      })
+      .then(() => {
+        console.log(postData);
+      });
+
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
