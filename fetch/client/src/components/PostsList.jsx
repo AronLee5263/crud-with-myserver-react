78bfc7a8f76c6d2dbee353f147172b135b0e0c6b
Aron <aronlee5263@gmail.com>
2023-06-01 01:38:52 +0900

    포스트 하나라도 있을때는 렌더링하도록 (이런 처리들이 필요함)

--- fetch/client/src/components/PostsList.jsx
+++ fetch/client/src/components/PostsList.jsx
@@ -20,11 +20,13 @@ export default function PostsList({ isEditing, onCloseModal }) {
         </Modal>
       ) : null}
 
-      <ul className={classes.posts}>
-        {posts.map((post) => (
-          <Post key={post.body} author={post.author} body={post.body} />
-        ))}
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
 
       {posts.length === 0 && (
         <div
