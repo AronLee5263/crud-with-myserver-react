08b39d572c1eed6eb466e8ffa74da1aa14a57425
Aron <aronlee5263@gmail.com>
2023-06-02 22:23:49 +0900

    더보기 버튼 구현 - 추가로 absolute 속성으로 모달창 업데이트 삭제 버튼 구현해야함

--- fetch/client/src/components/Post.jsx
+++ fetch/client/src/components/Post.jsx
@@ -1,10 +1,17 @@
 import classes from "./Post.module.css";
 
+import { MdMoreHoriz } from "react-icons/md";
+
 export default function Post({ author, body }) {
   return (
-    <li className={classes.post}>
-      <p className={classes.author}>{author}</p>
-      <p className={classes.body}>{body}</p>
-    </li>
+    <>
+      <li className={classes.post}>
+        <p className={classes.author}>{author}</p>
+        <p className={classes.body}>{body}</p>
+        <button className={classes.button}>
+          <MdMoreHoriz size={24} />
+        </button>
+      </li>
+    </>
   );
 }
