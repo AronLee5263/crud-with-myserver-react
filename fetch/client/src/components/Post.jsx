e1152c1225201a2035159d185359a7cc662e6a6d
Aron <aronlee5263@gmail.com>
2023-06-09 00:55:14 +0900

    Link 로 래핑 다시해줘여함 빌드를 위해서 잠시 Link 제거

--- fetch/client/src/components/Post.jsx
+++ fetch/client/src/components/Post.jsx
@@ -9,17 +9,11 @@ export default function Post({ id, author, body, onOpenPopup }) {
     <>
       {}
       <li className={classes.post}>
-        <Link to={id}>
-          <p className={classes.author}>{author}</p>
-          <p className={classes.body}>{body}</p>
-          <button
-            type="button"
-            className={classes.button}
-            onClick={onOpenPopup}
-          >
-            <MdMoreHoriz size={34} />
-          </button>
-        </Link>
+        <p className={classes.author}>{author}</p>
+        <p className={classes.body}>{body}</p>
+        <button type="button" className={classes.button} onClick={onOpenPopup}>
+          <MdMoreHoriz size={34} />
+        </button>
       </li>
     </>
   );
