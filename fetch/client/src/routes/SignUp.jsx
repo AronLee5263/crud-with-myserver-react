6315970695942a23b84c389734189df8eb1e0f5b
Aron <aronlee5263@gmail.com>
2023-06-12 00:33:22 +0900

    회원가입   sign-up
     email , password 폼 작성

--- fetch/client/src/routes/SignUp.jsx
+++ fetch/client/src/routes/SignUp.jsx
@@ -6,17 +6,27 @@ import BackButton from "../components/BackButton";
 
 export default function SignUp() {
   return (
-    <>
+    <div className={classes.main}>
       <div className={classes.topSection}>
         <BackButton />
+        <h1 className={classes.title}>회원가입 페이지</h1>
       </div>
       <div className={classes.middleSection}>
-        <h1 className={classes.title}>회원가입 페이지</h1>
+        <div>
+          {" "}
+          email : <input type="email" />{" "}
+        </div>
+        <div>
+          password : <input type="password" />
+        </div>
+        <button type="submit">회원가입 하기</button>
+      </div>
 
+      <div className={classes.bottomSection}>
         <p className={classes.text}>
           지금 사람들이 어떻게 디스크를 관리하는지 알아보세요!
         </p>
       </div>
-    </>
+    </div>
   );
 }
