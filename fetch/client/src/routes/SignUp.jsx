0c273c6e9c6be9fd05cbc3e040674eb9d2c96cbf
Aron <aronlee5263@gmail.com>
2023-06-12 00:50:48 +0900

    firebase 초기화  initialize
    및 config 구성 설정

--- fetch/client/src/routes/SignUp.jsx
+++ fetch/client/src/routes/SignUp.jsx
@@ -2,16 +2,31 @@ import classes from "./SignUp.module.css";
 
 import BackButton from "../components/BackButton";
 
-// import CrudButtons from "./components/CrudButtons";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 export default function SignUp() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDi6OwgJMjlIeqCbOsUvNxvUtJRVo9kZtk",
+    authDomain: "ddoclogin.firebaseapp.com",
+    projectId: "ddoclogin",
+    storageBucket: "ddoclogin.appspot.com",
+    messagingSenderId: "778162457671",
+    appId: "1:778162457671:web:2dbf7f3068026bf3073562",
+    measurementId: "G-8QFY6D5ML2",
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
   return (
     <div className={classes.main}>
       <div className={classes.topSection}>
         <BackButton />
         <h1 className={classes.title}>회원가입 페이지</h1>
       </div>
-      <div className={classes.middleSection}>
+
+      <form className={classes.middleSection}>
         <div>
           {" "}
           email : <input type="email" />{" "}
@@ -20,7 +35,7 @@ export default function SignUp() {
           password : <input type="password" />
         </div>
         <button type="submit">회원가입 하기</button>
-      </div>
+      </form>
 
       <div className={classes.bottomSection}>
         <p className={classes.text}>
