f5c810013cfc14c9f067ead488ebabb9e65e6dc8
Aron <aronlee5263@gmail.com>
2023-06-13 02:07:23 +0900

    로그인 회원가입 모두 성공  login, signup
    원래 하나의 firebase auth config 로 관리하려 했는데 아직 못했기 때문에 그냥 중복된 코드가 있음.

--- fetch/client/src/routes/Login.jsx
+++ fetch/client/src/routes/Login.jsx
@@ -1,10 +1,66 @@
 import classes from "./Login.module.css";
+import { useState } from "react";
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDi6OwgJMjlIeqCbOsUvNxvUtJRVo9kZtk",
+  authDomain: "ddoclogin.firebaseapp.com",
+  projectId: "ddoclogin",
+  storageBucket: "ddoclogin.appspot.com",
+  messagingSenderId: "778162457671",
+  appId: "1:778162457671:web:2dbf7f3068026bf3073562",
+  measurementId: "G-8QFY6D5ML2",
+};
 
 import BackButton from "../components/BackButton";
 
 // import CrudButtons from "./components/CrudButtons";
 
-export default function Login({ emailTypingHandler, passwordTypingHandler, singUpHandler }) {
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  function emailTypingHandler(e) {
+    setEmail(e.target.value);
+  }
+  function passwordTypingHandler(e) {
+    setPassword(e.target.value);
+  }
+
+  function loginHandler(event) {
+    event.preventDefault();
+    const auth = getAuth();
+    console.log(email, password);
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+
+        console.log("로그인이 성공했어요");
+        console.log("userCredential : ", userCredential);
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        console.log("로그인이 실패했어요 ");
+        console.log("error : ", error);
+
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
+
+  function showLogHandler(event) {
+    event.preventDefault();
+    console.log(app);
+  }
+
   return (
     <>
       <div className={classes.topSection}>
@@ -22,7 +78,7 @@ export default function Login({ emailTypingHandler, passwordTypingHandler, singU
         <div>
           password : <input type="password" onChange={passwordTypingHandler} id="loginPassword" />
         </div>
-        <button type="submit" onClick={singUpHandler} id="loginButton">
+        <button type="submit" onClick={loginHandler} id="loginButton">
           로그인 하기
         </button>
       </form>
--- fetch/client/src/routes/SignUp.jsx
+++ fetch/client/src/routes/SignUp.jsx
@@ -1,28 +1,26 @@
 import classes from "./SignUp.module.css";
 import { useState } from "react";
 
-import Login from "./Login";
-import BackButton from "../components/BackButton";
-
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import BackButton from "../components/BackButton";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDi6OwgJMjlIeqCbOsUvNxvUtJRVo9kZtk",
+  authDomain: "ddoclogin.firebaseapp.com",
+  projectId: "ddoclogin",
+  storageBucket: "ddoclogin.appspot.com",
+  messagingSenderId: "778162457671",
+  appId: "1:778162457671:web:2dbf7f3068026bf3073562",
+  measurementId: "G-8QFY6D5ML2",
+};
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDi6OwgJMjlIeqCbOsUvNxvUtJRVo9kZtk",
-    authDomain: "ddoclogin.firebaseapp.com",
-    projectId: "ddoclogin",
-    storageBucket: "ddoclogin.appspot.com",
-    messagingSenderId: "778162457671",
-    appId: "1:778162457671:web:2dbf7f3068026bf3073562",
-    measurementId: "G-8QFY6D5ML2",
-  };
-  // 이메일 @ 붙야사 제대로 입력하면 성공함
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
 
@@ -86,8 +84,6 @@ export default function SignUp() {
       <div className={classes.bottomSection}>
         <p className={classes.text}>지금 사람들이 어떻게 디스크를 관리하는지 알아보세요!</p>
       </div>
-
-      <Login onEmailTyping={emailTypingHandler} onPasswordTyping={passwordTypingHandler} onSignUp={singUpHandler} />
     </div>
   );
 }
