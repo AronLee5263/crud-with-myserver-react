938708245717de2e4879a1de06473e05a31e0a98
Aron <aronlee5263@gmail.com>
2023-06-13 23:05:33 +0900

    지연 로딩  /  lazy loading
    1. lazy 함수로 import 감싸기
    
    2.     Suspense로 컴포넌트 감싸기
            loader 속성에 error가 나올떄, loader의 값으로 전달 하는 함수의 매개변수로 params 혹은 meta가 빠졌는지 확인하기(이건 해당 매개변수 이름과 동일해야함)
    
    3.   에러발생 /  Cannot read properties of undefined (reading 'StrictMode')
           다음 커밋에서 처리예정

--- fetch/client/src/main.jsx
+++ fetch/client/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, lazy, Suspense, StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
@@ -6,14 +6,18 @@ import { RecoilRoot } from "recoil";
 
 import "./index.css";
 
-import App, { loader as postsLoader } from "./routes/App";
-import NewPost, { action as newPostAction } from "./routes/NewPost";
-import PostDetails, { loader as postDetailsLoader } from "./routes/PostDetails";
+// import App, { loader as postsLoader } from "./routes/App";
+// import NewPost, { action as newPostAction } from "./routes/NewPost";
+// import PostDetails, { loader as postDetailsLoader } from "./routes/PostDetails";
 
 import RootLayOut from "./routes/RootLayout";
 
 import Login from "./routes/Login";
 import SignUp from "./routes/SignUp";
+
+const App = lazy(() => import("./routes/App"));
+const NewPost = lazy(() => import("./routes/NewPost"));
+const PostDetails = lazy(() => import("./routes/PostDetails"));
 
 const router = createBrowserRouter([
   {
@@ -22,14 +26,30 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <App />,
-        loader: postsLoader,
+        element: (
+          <Suspense fallback={<p>로딩중입니다...</p>}>
+            <App />
+          </Suspense>
+        ),
+        loader: () => import("./routes/App").then((module) => module.loader()),
         children: [
-          { path: "/create_post", element: <NewPost />, action: newPostAction },
+          {
+            path: "/create_post",
+            element: (
+              <Suspense fallback={<p>로딩중입니다...</p>}>
+                <NewPost />
+              </Suspense>
+            ),
+            action: ({ request }) => import("./routes/NewPost").then((module) => module.action({ request })),
+          },
           {
             path: "/:postId",
-            element: <PostDetails />,
-            loader: postDetailsLoader,
+            element: (
+              <Suspense fallback={<p>로딩중입니다...</p>}>
+                <PostDetails />
+              </Suspense>
+            ),
+            loader: () => import("./routes/PostDetails").then((module) => module.loader()),
           },
         ],
       },
