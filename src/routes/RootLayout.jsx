22457ee772d776efc0b6d3c765c2a3ab22d8d2d7
Aron <aronlee5263@gmail.com>
2023-06-16 02:32:50 +0900

    로그인 회원가입 폼 삭제
    메인 페이지에서 삭제 프로필 클릭시 모달로 나타나게 변경할 예정

--- src/routes/RootLayout.jsx
+++ src/routes/RootLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import MainHeader from "../components/Mainheader";
 
@@ -9,14 +9,6 @@ export default function RootLayOut() {
     <>
       <MainHeader />
       <Outlet />
-      <div className={classes.buttons}>
-        <Link to="/login" className={classes.login}>
-          로그인
-        </Link>
-        <Link to="/sign_up" className={classes.sign_up}>
-          계정 만들기
-        </Link>
-      </div>
     </>
   );
 }
