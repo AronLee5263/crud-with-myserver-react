07459c97e422a8dde414e0d66a10ff682559c27e
Aron <aronlee5263@gmail.com>
2023-06-16 00:00:15 +0900

    재빌드 하고 재배포
    해야지만이 배포 사이트에 업데이트 할 수 있음.
    
    npm run build
    npm deploy

--- src/components/Mainheader.jsx
+++ src/components/Mainheader.jsx
@@ -1,12 +1,17 @@
 import { Link, Form } from "react-router-dom";
 import { MdPostAdd, MdMessage } from "react-icons/md";
 import { AiOutlinePlus } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
 
 import classes from "./MainHeader.module.css";
 
 function MainHeader() {
   return (
     <header className={classes.header}>
+      <div className={classes.profile_icon}>
+        <CgProfile size={24} className={classes.icon} />
+      </div>
+
       <div className={classes.category}>
         <h1 className={classes.community}>커뮤니티</h1>
         <h1 className={classes.goal}>달성기록</h1>
