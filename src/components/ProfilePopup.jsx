c1cbb9b5c9ebdc20fec4c383e66c55f2d74c9444
Aron <aronlee5263@gmail.com>
2023-06-16 15:37:29 +0900

    프로필 팝업  숨기기 기능 2
    - 뒤로가기 로고 클릭시 팝업 꺼지는 기능
    - profile Popup

--- src/components/ProfilePopup.jsx
+++ src/components/ProfilePopup.jsx
@@ -7,6 +7,7 @@ import { CgProfile } from "react-icons/cg";
 import { BsSun, BsBookmark } from "react-icons/bs";
 import { HiOutlineChatAlt2 } from "react-icons/hi";
 import { RiFileListLine } from "react-icons/ri";
+import { MdOutlineArrowBackIosNew } from "react-icons/md";
 
 // import { HiOutlineChatBubbleBottomCenterText } from "react-icons/hi";
 // import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
@@ -14,55 +15,57 @@ import { RiFileListLine } from "react-icons/ri";
 export default function ProfilePopup({ onCloseProfilePopup }) {
   return (
     <>
-      <div className={classes.backdrop} onClick={onCloseProfilePopup}>
-        <div className={classes.profilePopup}>
-          <div className={classes.sections}>
-            <div className={classes.headerSection}>
-              <p className={classes.mainTitle}>
-                지금 <span className={classes.diskDoctor}>disk doctor</span>에 가입하세요
-              </p>
+      <div className={classes.backdrop} onClick={onCloseProfilePopup} />
+      <div className={classes.profilePopup}>
+        <div className={classes.sections}>
+          <div className={classes.headerSection}>
+            <span className={classes.cancelIcon} onClick={onCloseProfilePopup}>
+              <MdOutlineArrowBackIosNew size={20} />
+            </span>
+            <p className={classes.mainTitle}>
+              지금 <span className={classes.diskDoctor}>disk doctor</span>에 가입하세요
+            </p>
 
-              <p className={classes.subTitle}>disk doctor를 설치하고 모든 기능을 이용해보세요</p>
-            </div>
-            <div className={classes.authSection}>
-              <Link to="/sign_up" className={classes.sign_up}>
-                <span className={classes.sign_up_text}>계정 만들기</span>
-              </Link>
-              <Link to="/login" className={classes.login}>
-                <span className={classes.login_text}>로그인</span>
-              </Link>
-            </div>
+            <p className={classes.subTitle}>disk doctor를 설치하고 모든 기능을 이용해보세요</p>
+          </div>
+          <div className={classes.authSection}>
+            <Link to="/sign_up" className={classes.sign_up}>
+              <span className={classes.sign_up_text}>계정 만들기</span>
+            </Link>
+            <Link to="/login" className={classes.login}>
+              <span className={classes.login_text}>로그인</span>
+            </Link>
+          </div>
 
-            <ul className={classes.categorySection}>
-              <li>
-                <CgProfile size={24} className={classes.icon} />
-                <span>프로필</span>
-              </li>
-              <li>
-                <HiOutlineChatAlt2 size={24} className={classes.icon} />
-                <span>커뮤니티</span>
-              </li>
-              <li>
-                <BsBookmark size={24} className={classes.icon} />
-                <span>북마크</span>
-              </li>
-              <li>
-                <RiFileListLine size={24} className={classes.icon} />
-                <span>리스트</span>
-              </li>
-            </ul>
+          <ul className={classes.categorySection}>
+            <li>
+              <CgProfile size={24} className={classes.icon} />
+              <span>프로필</span>
+            </li>
+            <li>
+              <HiOutlineChatAlt2 size={24} className={classes.icon} />
+              <span>커뮤니티</span>
+            </li>
+            <li>
+              <BsBookmark size={24} className={classes.icon} />
+              <span>북마크</span>
+            </li>
+            <li>
+              <RiFileListLine size={24} className={classes.icon} />
+              <span>리스트</span>
+            </li>
+          </ul>
 
-            <div className={classes.settingSection}>
-              <span>설정 및 지원</span>
-              <span className={classes.settingIcon}>
-                <RiArrowDownSLine size={30} />
-              </span>
-            </div>
-            <div className={classes.screenTheme}>
-              <button type="button" className={classes.screenTheme_button}>
-                <BsSun size={20} />
-              </button>
-            </div>
+          <div className={classes.settingSection}>
+            <span>설정 및 지원</span>
+            <span className={classes.settingIcon}>
+              <RiArrowDownSLine size={30} />
+            </span>
+          </div>
+          <div className={classes.screenTheme}>
+            <button type="button" className={classes.screenTheme_button}>
+              <BsSun size={20} />
+            </button>
           </div>
         </div>
       </div>
