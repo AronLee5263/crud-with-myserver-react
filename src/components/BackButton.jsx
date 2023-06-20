fb9eb2a8a82705ca725773f909d26850ef278f56
Aron <aronlee5263@gmail.com>
2023-06-20 16:13:04 +0900

    back 버튼 컴포넌트 화

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,13 +1,24 @@
 import classes from "./BackButton.module.css";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function BackButton() {
+  let navigate = useNavigate();
+
   return (
     <>
       <Link to="../" type="button" className={classes.back_button_container}>
         <span className={classes.text}> 취소</span>
       </Link>
+
+      {/* <button
+        type="button"
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        이전 페이지
+      </button> */}
     </>
   );
 }
