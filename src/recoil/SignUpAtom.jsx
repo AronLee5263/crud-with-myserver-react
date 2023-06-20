be333b6b69ef7c0c61b6601e3773f6e8d1829993
Aron <aronlee5263@gmail.com>
2023-06-20 18:13:29 +0900

    recoil 선언

--- src/recoil/SignUpAtom.jsx
+++ src/recoil/SignUpAtom.jsx
@@ -1,27 +1,32 @@
 import { atom } from "recoil";
 
-export const signUpWithPassword = atom({
+export const isClickSignUpWithPassword = atom({
   key: "isClickSignUpWithPassword",
-  default: [],
+  default: false,
 });
 
-export const signUpWithEmailLink = atom({
+export const isClickSignUpWithEmailLink = atom({
   key: "isClickSignUpWithEmailLink",
-  default: [],
+  default: false,
 });
 
 export const userEmail = atom({
   key: "userEmail",
-  default: [""],
+  default: "",
 });
 
 export const userNickName = atom({
   key: "userNickName",
-  default: [""],
+  default: "",
 });
 
-const myNewSelector = selector({
-  key: "myNewSelector",
-  get: ({ get }) => get(myAtom),
-  set: ({ set }, newValue) => set(myAtom, newValue),
+export const textState = atom({
+  key: "textState", // unique ID (with respect to other atoms/selectors)
+  default: "", // default value (aka initial value)
 });
+
+// const myNewSelector = selector({
+//   key: "myNewSelector",
+//   get: ({ get }) => get(myAtom),
+//   set: ({ set }, newValue) => set(myAtom, newValue),
+// });
