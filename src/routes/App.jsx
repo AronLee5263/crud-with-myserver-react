833a5126b76775d5d43e8dd22e4eb658c63fb4c5
Aron <aronlee5263@gmail.com>
2023-06-22 19:01:49 +0900

    firebase 데이터 가져오기

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,18 @@
 import classes from "./App.module.css";
+
+import { useEffect, useState } from "react";
+
+import { Outlet } from "react-router-dom";
+
 import axios from "axios";
 
 import PostsList from "../components/PostsList";
-import { Outlet } from "react-router-dom";
 
 const SERVER_URL = "https://64637a9f7a9eead6fae801e2.mockapi.io/fakeData";
 
 export default function App() {
+  const [books, setBooks] = useState(null);
+  
   return (
     <>
       <Outlet />
@@ -16,6 +22,7 @@ export default function App() {
     </>
   );
 }
+
 
 export async function loader() {
   const response = await axios.get(SERVER_URL);
