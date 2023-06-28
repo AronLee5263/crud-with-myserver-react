76572d94482596e9a53b697d37825ac712569258
Aron <aronlee5263@gmail.com>
2023-06-29 04:21:48 +0900

    아 고민 개많이 했다.
    context 사용하기 개빡세네,,
    
    newPost 에서 클릭했을떄만 state 값을 true로 보내니까
    기본 루트 레이아웃 컴포넌트에서는 항상 값을 true로 해줘야했음.
    
    이게 아니라 값을 보내는 여부랑 상관없는 렌더링 조건식이 필요했음.
    
    << 해결했다. >
    
    메인 헤더가 안보여주게 하려고 조건식을 썼던건데 그냥 NewPost 컴포넌트의 z index값을 더 위로주고 position을 젤 위로 하면됨. 그럼 스크롤 안되고
    본인만 보임.
    
    - 그러니까 NewPost 에서 Link 로 state 보낼 필요가 없는거지. 보내는 부분 전부 삭제하기

--- src/routes/pages/WrapperRootLayOut.jsx
+++ src/routes/pages/WrapperRootLayOut.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import RootLayOut from "../pages/RootLayout";
+
+import NewPostAnimationContext from "../../store/newPost-animation-context";
 
 export default function WrapperRootLayOut() {
   return (
     <>
-      <RootLayOut />
+      <NewPostAnimationContext.Provider
+        value={{
+          isClickedNewPost: false,
+        }}
+      >
+        <RootLayOut />
+      </NewPostAnimationContext.Provider>
     </>
   );
 }
--- src/store/newPost-animation-context.js
+++ src/store/newPost-animation-context.js
@@ -2,6 +2,11 @@ import React from "react";
 
 const NewPostAnimationContext = React.createContext({
   isClickedNewPost: false,
+  isClickedCancelBtn: true,
 });
 
 export default NewPostAnimationContext;
+
+// export const cancelNewPostBtn = React.createContext({
+//   isClickedCancelBtn: true,
+// });
