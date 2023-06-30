811c9b0cdd61e1b1bba7f7f6979d62af335e6395
Aron <aronlee5263@gmail.com>
2023-06-30 19:04:31 +0900

    전체화면시 스크롤 조금씩 생기는 문제 수정
    100vw > 100% 변경
    
    vh는 부모랑 상관없이 100%
    %는 부모에 영향받음

--- src/components/NoMobile.jsx
+++ src/components/NoMobile.jsx
@@ -3,13 +3,13 @@ import classes from "./NoMobile.module.css";
 export default function NoMobile() {
   return (
     <>
-      <div className={classes.no_mobile}>
+      {/* <div className={classes.no_mobile}>
         <span className={classes.text}>
           스크린이 너무 커서 볼 수 없어요. <br />
           <br />
           휴대폰이나 작은 화면에서 실행해주세요
         </span>
-      </div>
+      </div> */}
     </>
   );
 }
