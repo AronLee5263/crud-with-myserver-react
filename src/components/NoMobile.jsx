e68c6546db384ed0a562f1af5565bd8d6c40badb
Aron <aronlee5263@gmail.com>
2023-07-05 19:11:17 +0900

    320 px 보다 작을떄 안보이게 미디어쿼리변경

--- src/components/NoMobile.jsx
+++ src/components/NoMobile.jsx
@@ -3,13 +3,26 @@ import classes from "./NoMobile.module.css";
 import { useMediaQuery } from "react-responsive";
 
 export default function NoMobile() {
+  const small = useMediaQuery({ query: "(max-width: 319px) " });
+  const large = useMediaQuery({ query: "(min-width: 645px) " });
+
+  let text;
+  let text2;
+
+  if (small) {
+    text = "스크린이 너무 작아서 볼 수 없어요.";
+    text2 = "좀 더 큰 화면에서 실행해주세요 ";
+  } else if (large) {
+    text = "스크린이 너무 커서 볼 수 없어요.";
+    text2 = "휴대폰이나 작은 화면에서 실행해주세요 ";
+  }
   return (
     <>
       <div className={classes.no_mobile}>
         <span className={classes.text}>
-          스크린이 너무 커서 볼 수 없어요. <br />
+          {text} <br />
           <br />
-          휴대폰이나 작은 화면에서 실행해주세요
+          {text2}
         </span>
       </div>
     </>
@@ -17,6 +30,6 @@ export default function NoMobile() {
 }
 
 export function UseIsMobile() {
-  const noMobileSize = useMediaQuery({ query: "(max-width: 645px)" });
+  const noMobileSize = useMediaQuery({ query: "(min-width: 320px) and (max-width: 645px)" });
   return noMobileSize;
 }
